7b03750a0ac07a1a99506083aa69430c0e5ba6ce
Scott Wambach <scott.wambach@redstitchdigital.com>
2018-06-20 20:16:29 -0500

    instagram

--- source/javascripts/core/base.js
+++ source/javascripts/core/base.js
@@ -1,4 +1,5 @@
 const $ = require('jquery');
+const Instafeed = require('instafeed.js');
 
 function getYear() {
   const date = new Date();
@@ -9,3 +10,11 @@ function getYear() {
 $(document).ready(() => {
   getYear();
 });
+
+const feed = new Instafeed({
+  get: 'tagged',
+  tagName: 'awesome',
+  clientId: '8d733874853341d2bec0b929411b75f4',
+  accessToken: '12752195.1677ed0.8274541354a84f12a7bfc80f1f16a452',
+});
+feed.run();
