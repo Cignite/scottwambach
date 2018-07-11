12b2ddcef84b4d1419274428cf107cecb0ec5ddd
Scott Wambach <scott.wambach@redstitchdigital.com>
2018-07-11 11:44:38 -0500

    barba optimize

--- source/js/modules/base.js
+++ source/js/modules/base.js
@@ -1,3 +1,8 @@
 const Barba = require('barba.js');
 
+Barba.Pjax.Dom.wrapperId = 'body-container';
+Barba.Pjax.Dom.containerClass = 'inner-container';
+
 Barba.Pjax.start();
+
+Barba.Prefetch.init();
